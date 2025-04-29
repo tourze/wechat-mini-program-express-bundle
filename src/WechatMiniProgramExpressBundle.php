060f262d0ccc8e92bbb061f98dd29f3c4503cbc9
github-ci <you@example.com>
2025-04-29 13:13:35 +0000

    refactor(bundles): update

--- src/WechatMiniProgramExpressBundle.php
+++ src/WechatMiniProgramExpressBundle.php
@@ -3,16 +3,7 @@
 namespace WechatMiniProgramExpressBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '即时配送')]
-class WechatMiniProgramExpressBundle extends Bundle implements BundleDependencyInterface
+class WechatMiniProgramExpressBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatMiniProgramBundle\WechatMiniProgramBundle::class => ['all' => true],
-        ];
-    }
 }
